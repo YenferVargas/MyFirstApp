4a7543859140d1d54218c8b70c05fad8cca5e6df
chenaoch <cdhenao@gmail.com>
2023-04-07 08:02:41 -0500

    Initial commit

---
+++ app/src/main/java/tecsup/edu/pe/SplashActivity.kt
@@ -0,0 +1,19 @@
+package tecsup.edu.pe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
